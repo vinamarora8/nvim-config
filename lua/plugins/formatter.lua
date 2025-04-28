b7f89c97643cbfafec91b197b4c54016adf01da1
Vinam Arora <vinam@posteo.de>
2025-04-28 03:30:16 -0400

    format on save

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,6 +2,10 @@ return {
     {
         "stevearc/conform.nvim",
         opts = {
+            format_on_save = {
+                lsp_format = "fallback",
+                timeout_ms = 500,
+            },
             formatters_by_ft = {
                 python = { "black" },
                 lua = { "stylua" },
