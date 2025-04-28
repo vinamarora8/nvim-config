63c08ab5917f62a9cfbd69bb99d48ac753064635
Vinam Arora <vinam@posteo.de>
2025-04-27 21:54:40 -0400

    set number

--- lua/set.lua
+++ lua/set.lua
@@ -5,6 +5,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "both"
