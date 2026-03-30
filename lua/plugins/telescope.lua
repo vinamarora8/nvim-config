b004bdc83d26bda480638de03b307824676aae3b
Vinam Arora <vinam@seas.upenn.edu>
2026-03-30 00:43:51 -0400

    Nav in telescope with C-j/C-k

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,8 @@ return {
                 mappings = {
                     i = {
                         ["<esc>"] = require("telescope.actions").close, -- Single escape exit
+                        ["<C-j>"] = require('telescope.actions').move_selection_next,
+                        ["<C-k>"] = require('telescope.actions').move_selection_previous,
                     },
                 },
             },
