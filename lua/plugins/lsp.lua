1c88954d61c67c8aed908af1768fd939de25f3cb
Vinam Arora <vinam@seas.upenn.edu>
2026-03-31 16:44:42 -0400

    lsp cleanup with blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,18 +3,10 @@ return {
   dependencies = {
     "mason-org/mason.nvim",
     "mason-org/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
     "j-hui/fidget.nvim",
+    "saghen/blink.cmp",
   },
-
   config = function()
-    local cmp = require("cmp")
-    local cmp_lsp = require("cmp_nvim_lsp")
-
     require("fidget").setup({})
     require("mason").setup()
     require("mason-lspconfig").setup({
@@ -24,6 +16,9 @@ return {
         "stylua",
       },
     })
+
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+    vim.lsp.config("*", { capabilities = capabilities })
 
     -- ty: not yet in mason-lspconfig, configure directly
     vim.lsp.enable("ty")
@@ -39,21 +34,6 @@ return {
       },
     })
 
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-    cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-      }),
-    })
-
     vim.diagnostic.config({
       update_in_insert = true,
       virtual_text = true,
@@ -61,7 +41,7 @@ return {
         focusable = false,
         style = "minimal",
         border = "rounded",
-        source = "always",
+        source = "if_many",
         header = "",
         prefix = "",
       },
